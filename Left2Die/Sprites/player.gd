6938f3c81057d11e6dd59dd2591df83d596a7438
Harpod389 <harpod55@kingshigh.school.nz>
2024-07-05 16:06:31 +1200

    Basic gun animations
    I added the basic animations for my shooting mechanic.

--- Left2Die/Sprites/player.gd
+++ Left2Die/Sprites/player.gd
@@ -5,16 +5,18 @@ extends CharacterBody2D
 @export var ACCELERATION = 20 
 @export var FRICTION = 10.0
 @onready var sprite = $Sprite
-
+const BULLET = preload("res://Scenes/Bullet.tscn")
 func check_animation():
 	if velocity == Vector2.ZERO:
 		sprite.play("Idle")
 	elif velocity.x != 0:
 		sprite.play("Running")
+	if Input.is_action_pressed("shoot"):
+		sprite.play("Shooting")
+	
  
 
 func _physics_process(delta):
-	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_vector("left","right","up","down").normalized()
@@ -28,6 +30,24 @@ func _physics_process(delta):
 	elif velocity.x < 0: 
 		sprite.flip_h = true 
 
+	if Input.is_action_just_pressed("shoot"):
+		var new_bullet = BULLET.instantiate()
+		new_bullet.global_position = global_position
+		new_bullet.look_at(get_global_mouse_position())
+		add_sibling(new_bullet)
+		
+
+		
+			
 
 	check_animation()
 	move_and_slide()
+
+
+
+
+
+
+
+
+
