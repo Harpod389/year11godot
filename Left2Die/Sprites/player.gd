aad90afd4eba04d2a01847f0d183f7dddefa2b2f
Harpod389 <harpod55@kingshigh.school.nz>
2024-08-14 14:03:16 +1200

    Worked on the player damage system. This included registering the collision with the enemy and playing the damage animation, and also the death animation once health is zero

--- Left2Die/Sprites/player.gd
+++ Left2Die/Sprites/player.gd
@@ -10,7 +10,7 @@ extends CharacterBody2D
 @onready var gunshots = $Gunshots
 @onready var collision_shape = $HurtBox
 const DAMAGE_RATE = 5.0
-
+var died = false
 
 
 const BULLET = preload("res://Scenes/Bullet.tscn")
@@ -24,44 +24,42 @@ func check_animation():
 	
 
 func get_damage(amount):
+	if died:
+		return
 	health -= amount
 	print(health)
 	$Sprite.play("Damage")
-	if amount <= 0:
+	if health <= 0:
+		print("i died")
 		$Sprite.play("Death")
+		died = true
+		
 
  
 
 func _physics_process(delta):
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_vector("left","right","up","down").normalized()
-	if direction:
-		velocity = velocity.move_toward(direction * SPEED, ACCELERATION)
-	else:
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
-	
-	if velocity.x > 0: 
-		sprite.flip_h = false 
-	elif velocity.x < 0: 
-		sprite.flip_h = true 
+	if died == true:
+		var direction = Input.get_vector("left","right","up","down").normalized()
+		if direction:
+			velocity = velocity.move_toward(direction * SPEED, ACCELERATION)
+		else:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
+			if velocity.x > 0: 
+						sprite.flip_h = false 
+			elif velocity.x < 0: 
+						sprite.flip_h = true 
 
 
 
+		if Input.is_action_just_pressed("shoot"):
+						var new_bullet = BULLET.instantiate()
+						new_bullet.global_position = global_position
+						new_bullet.look_at(get_global_mouse_position())
+						add_sibling(new_bullet)
+						gunshots.play()
 
-
-	if Input.is_action_just_pressed("shoot"):
-		var new_bullet = BULLET.instantiate()
-		new_bullet.global_position = global_position
-		new_bullet.look_at(get_global_mouse_position())
-		add_sibling(new_bullet)
-		gunshots.play()
-		
-		
-
-
-	check_animation()
-	move_and_slide()
+		check_animation()
+		move_and_slide()
 
 
 
