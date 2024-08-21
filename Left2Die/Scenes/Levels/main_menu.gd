d4990deb22f5f0dde0f6cd4643498f5f84756c80
Harpod389 <harpod55@kingshigh.school.nz>
2024-08-21 13:27:06 +1200

    21/08/24
    Created and finished the title screen and also added collision boundaries to the main world

---
+++ Left2Die/Scenes/Levels/main_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _ready():
+	pass 
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Levels/world.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
